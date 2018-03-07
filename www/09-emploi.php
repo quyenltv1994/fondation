f326e37e6404f80a43cf733410c4da180be557b4
Quyen Le <quyenltv54cntt@gmail.com>
2018-03-07 15:41:04 +0700

    emploi

---
+++ www/09-emploi.php
@@ -0,0 +1,258 @@
+<?php $class = 'home'; ?>
+<?php include 'header.php'; ?>
+    <div class="main-image cover" style="background-image: url('http://fondationaf.havas.staging.fides.io/sites/default/files/header_emploi.jpg');">
+    </div>
+    <div class="wrapper main-content">
+
+        <div class="">
+
+            <!-- Begin share block -->
+            <div class="fixed-menu">
+                <div class="wrapper">
+                    <div class="section-holder">
+
+                        <section class="sub-block">
+                            <div class="social-box">
+                                <div class="content">
+                                    <span>Partagez</span>
+                                    <!-- social networks -->
+                                    <ul class="social-networks">
+                                        <li><a href="https://www.facebook.com/sharer/sharer.php?p[title]=Emploi&amp;p[summary]=&amp;p[url]=http://fondationaf.havas.staging.fides.io//fr/emploi" class="social-icon facebook share-popup"></a></li>
+                                        <li><a href="https://twitter.com/home?status=Emploi+-+http%3A%2F%2Ffondationaf.havas.staging.fides.io%2F%2Ffr%2Femploi+-+Air+France+%0D%0APour+en+savoir+plus+%3A+http%3A%2F%2Fcorporate.airfrance.com%2Ffr%2F" class="social-icon twitter share-popup"></a></li>
+                                        <li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=http://fondationaf.havas.staging.fides.io//fr/emploi&amp;title=Emploi&amp;summary=" class="social-icon linkedin share-popup"></a></li>
+                                        <li>
+                                            <a class="icon-print">
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+            <!-- End share block -->
+        </div>
+
+        <!-- contain the main content of the page -->
+        <div class="container">
+            <div class="page-content">
+
+
+                <div class="actu-simple">
+                    <div class="region region-content">
+
+                        <section id="block-system-main" class=" block block-system contextual-links-region">
+
+                            <div class="editorial-intro">
+                                <div class="block--content">
+                                    <h1>Emploi</h1>
+                                </div>
+                            </div>
+
+
+                            <div class="mosaic mosaic--hub">
+                                <div class="mosaic__content">
+
+
+
+
+                                    <div class="block block--double" style="background-image:url('http://fondationaf.havas.staging.fides.io/sites/default/files/styles/hp_block_double_632x284/public/hub_pourquoi_af.jpg?itok=sYRhRnYw');">
+                                        <div class="block--inner">
+                                            <div class="block--content">
+
+                                            </div>
+
+                                            <div class="morelink">
+                                                <a href="http://fondationaf.havas.staging.fides.io/fr/pourquoi-air-france" target="_self" class="">
+                                                    Pourquoi air france ?                        </a>
+                                            </div>
+
+
+
+
+                                        </div>
+                                    </div>
+
+
+                                    <div class="block block--simple" style="background-image:url('http://fondationaf.havas.staging.fides.io/sites/default/files/styles/hp_block_simple_308x284/public/nous_rejoindre2.png?itok=qLkhD2le');">
+                                        <div class="block--inner">
+                                            <div class="block--content">
+                                                <h2>
+                                                    <div class="field field-name-field-title field-type-text field-label-hidden">
+                                                        <div class="field-items">
+                                                            <div class="field-item even">rejoignez-nous</div>
+                                                        </div>
+                                                    </div>                        </h2>
+
+                                                <div class="description">
+                                                    <div class="field field-name-field-description field-type-text-long field-label-hidden">
+                                                        <div class="field-items">
+                                                            <div class="field-item even">Découvrez nos offres de recrutement.</div>
+                                                        </div>
+                                                    </div>                        </div>
+                                            </div>
+
+                                            <div class="morelink">
+                                                <a href="http://recrutement.airfrance.com/accueil.aspx" target="_blank" class="red">
+                                                    découvrez nos offres                        </a>
+                                            </div>
+
+
+
+
+                                        </div>
+                                    </div>
+
+                                    <div class="block block--simple" style="background-image:url('http://fondationaf.havas.staging.fides.io/sites/default/files/styles/hp_block_simple_308x284/public/com-p072466.jpg?itok=0IMmnATn');">
+                                        <div class="block--inner">
+                                            <div class="block--content">
+
+                                            </div>
+
+                                            <div class="morelink">
+                                                <a href="https://air-france.career-inspiration.com/app/home/land:177@5lEoCMc" target="_blank" class="">
+                                                    Nos experts vous répondent                        </a>
+                                            </div>
+
+
+
+
+                                        </div>
+                                    </div>
+
+                                    <div class="block block--double" style="background-image:url('http://fondationaf.havas.staging.fides.io/sites/default/files/styles/hp_block_double_632x284/public/4_hub_emploi.png?itok=g6YQw39W');">
+                                        <div class="block--inner">
+                                            <div class="block--content">
+
+                                            </div>
+
+
+
+
+                                            <div class="list-of-links">
+                                                <div class="title">
+                                                    Nos métiers                            </div>
+                                                <div class="list">
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/entretien-aeronautique" target="_self">
+                                                        Entretien aéronautique                                        </a>
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/autres-metiers-supports" target="_self">
+                                                        Autres métiers supports                                        </a>
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/strategie-et-developpement" target="_self">
+                                                        Stratégie &amp; développement                                        </a>
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/conseil-aux-clients" target="_self">
+                                                        Conseil au client                                        </a>
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/systemes-dinformation" target="_self">
+                                                        Système d'information                                        </a>
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/pilote" target="_self">
+                                                        Pilotes                                        </a>
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/digital-0" target="_self">
+                                                        Digital                                        </a>
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/personnel-navigant-commercial-0" target="_self">
+                                                        Personnel Navigant Commercial                                        </a>
+
+
+                                                    <a href="http://fondationaf.havas.staging.fides.io/fr/fret-et-logistique" target="_self">
+                                                        Frêt &amp; logistique                                        </a>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="block block--double" style="background-image:url('http://fondationaf.havas.staging.fides.io/sites/default/files/styles/hp_block_double_632x284/public/photo_path_motion.jpg?itok=K4rjGv4u');">
+                                        <div class="block--inner">
+                                            <div class="block--content">
+
+                                            </div>
+
+                                            <div class="morelink">
+                                                <a href="http://fondationaf.havas.staging.fides.io/fr/jeunes-diplomes" target="_self" class="">
+                                                    jeunes diplômés                        </a>
+                                            </div>
+
+
+
+
+                                        </div>
+                                    </div>
+
+                                    <div class="block block--simple" style="background-image:url('http://fondationaf.havas.staging.fides.io/sites/default/files/styles/hp_block_simple_308x284/public/dxvo-p051164.jpg?itok=M9vkmoQV');">
+                                        <div class="block--inner">
+                                            <div class="block--content">
+
+                                            </div>
+
+                                            <div class="morelink">
+                                                <a href="http://fondationaf.havas.staging.fides.io/fr/etudiants" target="_self" class="">
+                                                    ÉTUDIANTS                        </a>
+                                            </div>
+
+
+
+
+                                        </div>
+                                    </div>
+
+                                    <div class="block block--simple" style="background-image:url('http://fondationaf.havas.staging.fides.io/sites/default/files/styles/hp_block_simple_308x284/public/nos_video4_bloc_simple.png?itok=fPDvrSsR');">
+                                        <div class="block--inner">
+                                            <div class="block--content">
+
+                                            </div>
+
+                                            <div class="morelink">
+                                                <a href="https://www.youtube.com/playlist?list=PLEAD7518FBBDF93CC" target="_blank" class="">
+                                                    Nos vidéos métiers                        </a>
+                                            </div>
+
+
+
+
+                                        </div>
+                                    </div>
+
+                                    <div class="block block--double" style="background-image:url('http://fondationaf.havas.staging.fides.io/sites/default/files/styles/hp_block_double_632x284/public/faq_0.png?itok=Ki2MvJy1');">
+                                        <div class="block--inner">
+                                            <div class="block--content">
+
+                                            </div>
+
+                                            <div class="morelink">
+                                                <a href="http://fondationaf.havas.staging.fides.io/fr/faq" target="_self" class="">
+                                                    FAQ                          </a>
+                                            </div>
+
+
+
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+                        </section>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+<?php include 'footer.php'; ?>
