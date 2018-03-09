30919cfc95951f8a175337f474e86e77618d31f6
Quyen Le <quyenltv54cntt@gmail.com>
2018-03-09 15:35:55 +0700

    update presse home

--- www/blocks/presse_hone.php
+++ www/blocks/presse_hone.php
@@ -4,7 +4,7 @@
             <a href="" class="read__more">Toutes les ressources média</a>
         </h2>
         <div class="presse__home--main">
-            <a href="" class="presse__home--item">
+            <div class="presse__home--item">
                 <div class="image">
                     <img src="../assets/img/presse__home-img.jpg" alt="">
                 </div>
@@ -13,45 +13,46 @@
                         Rapport d’activité
                             2017-2018
                     </p>
-                    <p><span class="read__more">en savoir plus</span></p>
+                    <p><span href="" class="read__more">en savoir plus</span></p>
                 </div>
-            </a>
-            <a href="" class="presse__home--item">
+                <a href="">en savoir plus</a>
+            </div>
+            <div class="presse__home--item">
                 <div class="image">
                     <img src="../assets/img/presse__home-img.jpg" alt="">
                 </div>
                 <div class="presse__home--item--content">
                     <p class="presse__home--item-title">
-                        Rapport d’activité
-                        2017-2018
+                        <a href="">Rapport d’activité
+                            2017-2018</a>
                     </p>
-                    <p><span class="read__more">en savoir plus</span></p>
+                    <p><a href="" class="read__more">en savoir plus</a></p>
                 </div>
-            </a>
-            <a href="" class="presse__home--item">
+            </div>
+            <div class="presse__home--item">
                 <div class="image">
                     <img src="../assets/img/presse__home-img.jpg" alt="">
                 </div>
                 <div class="presse__home--item--content">
                     <p class="presse__home--item-title">
-                        Rapport d’activité
-                        2017-2018
+                        <a href="">Rapport d’activité
+                            2017-2018</a>
                     </p>
-                    <p><span class="read__more">en savoir plus</span></p>
+                    <p><a href="" class="read__more">en savoir plus</a></p>
                 </div>
-            </a>
-            <a href="" class="presse__home--item">
+            </div>
+            <div class="presse__home--item">
                 <div class="image">
                     <img src="../assets/img/presse__home-img.jpg" alt="">
                 </div>
                 <div class="presse__home--item--content">
                     <p class="presse__home--item-title">
-                        Rapport d’activité
-                        2017-2018
+                        <a href="">Rapport d’activité
+                            2017-2018</a>
                     </p>
-                    <p><span class="read__more">en savoir plus</span></p>
+                    <p><a href="" class="read__more">en savoir plus</a></p>
                 </div>
-            </a>
+            </div>
         </div>
     </div>
 </div>
